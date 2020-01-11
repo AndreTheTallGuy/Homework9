a0bee515e92f2d4c909fa8a7d4412d2b8ab5fee8
Andre Entrekin <andre.entrekin@gmail.com>
2020-01-11 14:08:36 -0600

    worker timeout error

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const inquirer = require("inquirer")
 const fs = require("fs")
 const util = require("util")
 const axios = require("axios")
-module.exports = require("./generateHTML.js")
+const generateHTML = require('./generateHTML.js')
 const electron = require('electron')
 const proc = require('child_process')
-
-
+const convertFactory = require('electron-html-to');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
-
 inquirer
     .prompt([
         {
@@ -28,11 +26,13 @@ inquirer
                 "red"
             ]
         },
-
     ])
     .then(function ({ username }) {
+        console.log(username);
+        console.log(`====================================`);
         const queryUrl = `https://api.github.com/users/${username}`;
         axios.get(queryUrl).then(function (res) {
+            console.log(`axios get is successful`);
             const name = res.data.name;
             const company = res.data.company;
             const photo = res.data.avatar_url //photo
@@ -44,35 +44,37 @@ inquirer
             const repos = res.data.public_repos; //repos
             const followers = res.data.followers; // followers
             const following = res.data.following; // following
+            console.log(`all these consts`);
+            var conversion = convertFactory({
+                converterPath: convertFactory.converters.PDF
+            });
 
 
+
+
+
+            console.log('conversion declared')
+            conversion({
+                html: generateHTML
+            }, function (err, result) {
+                console.log(`conversion callback started`)
+                if (err) {
+                    return console.error(err);
+                }
+                console.log(result.numberOfPages);
+                console.log(result.logs);
+                result.stream.pipe(fs.createWriteStream('test.pdf'));
+                conversion.kill();
+            });
         })
-
+        // .then(function ({ username }) {
+        //     const queryUrl = `https://api.github.com/users/${username}/starred`;
+        //     axios.get(queryUrl).then(function (res) {
+        //         // const stars
+        //         console.log(res);
+        //     })
+        // })
     })
-// .then(function ({ username }) {
-//     const queryUrl = `https://api.github.com/users/${username}/starred`;
-//     axios.get(queryUrl).then(function (res) {
-//         // const stars
-//         console.log(res.length());
-//     })
-// })
 
 
 // github api: 4ff665d703e6da2dada3227637504bd27d6c6c44
-
-convertFactory = require('electron-html-to');
-
-var conversion = convertFactory({
-    converterPath: convertFactory.converters.PDF
-});
-
-conversion({ html: '<h1>Hello World</h1>' }, function (err, result) {
-    if (err) {
-        return console.error(err);
-    }
-
-    console.log(result.numberOfPages);
-    console.log(result.logs);
-    result.stream.pipe(fs.createWriteStream('test.pdf'));
-    conversion.kill();
-});
